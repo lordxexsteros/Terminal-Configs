da586e90b5cac78ead0e71f8239d53bac6aab8c2
Nox <code.xexsteros@gmail.com>
2024-08-08 14:59:50 +0530

    init files

---
+++ Configs/Bashrc.sh
@@ -0,0 +1,12 @@
+############################################################
+# Simple (TM) Bash Settings
+# By Mayank
+###########################################################
+
+[ -z "$PS1" ] && return
+
+
+
+eval "$(starship init bash)"
+neofetch
+echo  Wellcome to StarShip! Sir. ---
